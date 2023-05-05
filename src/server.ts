43b1b7bd5964d7153c69b59326e146f02aadf045
anammSky <ana.morosmarmol@sky.uk>
2023-05-05 17:10:56 +0100

    init db

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import "./utils/env";
 import "./sockets";
 import server from "./app";
+import { initDatabase } from "./db";
 
 const port = process.env.PORT;
+
+await initDatabase();
 
 server.listen(port, () => {
 	console.log(`[Server]: Server is running at http://localhost:${port}`);
