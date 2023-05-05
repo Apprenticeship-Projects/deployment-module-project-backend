71a4be07266895a745a34560da75ba0877c09fb4
anammSky <ana.morosmarmol@sky.uk>
2023-05-05 16:39:34 +0100

    added planned routes

--- src/routes/channelRoute.ts
+++ src/routes/channelRoute.ts
@@ -2,6 +2,18 @@ import { Router } from "express";
 
 export const channelRoute = Router();
 
-channelRoute.get('/', (req, res) => {
-    res.send("This is the channel route")
+channelRoute.post('/create', (req, res) => {
+    res.send("This is the channel/create POST route")
+})
+
+channelRoute.get('/{id}', (req, res) => {
+    res.send("This is the channel/{id} GET route")
+})
+
+channelRoute.put('/{id}', (req, res) => {
+    res.send("This is the channel/{id} PUT route")
+})
+
+channelRoute.delete('/{id}', (req, res) => {
+    res.send("This is the channel/{id} DELETE route")
 })
--- src/routes/messageRoute.ts
+++ src/routes/messageRoute.ts
@@ -2,6 +2,18 @@ import { Router } from "express";
 
 export const messageRoute = Router();
 
-messageRoute.get('/', (req, res) => {
-    res.send("This is the message route")
+messageRoute.get('/{id}', (req, res) => {
+    res.send("This is the message/{id} GET route")
+})
+
+messageRoute.post('/{id}', (req, res) => {
+    res.send("This is the message/{id} POST route")
+})
+
+messageRoute.put('/{id}', (req, res) => {
+    res.send("This is the message/{id} POST route")
+})
+
+messageRoute.delete('/{id}', (req, res) => {
+    res.send("This is the message/{id} DELETE route")
 })
