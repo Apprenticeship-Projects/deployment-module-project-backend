4f482f52142343ba2c5e18d16f0f6fe92d123c5a
anammSky <ana.morosmarmol@sky.uk>
2023-05-05 17:11:31 +0100

    message get all

--- src/routes/messageRoute.ts
+++ src/routes/messageRoute.ts
@@ -1,14 +1,29 @@
 import { Router } from "express";
+import { Message } from "../db/models";
 
 export const messageRoute = Router();
+
+messageRoute.post('/create', async (req, res) => {
+    try {
+        const message = await Message.create(req.body)
+        res.status(201).send(message)
+    } catch (err) {
+        res.status(500).send({error: err})
+    }
+
+    res.send("This is the message/{id} POST route")
+})
+
+messageRoute.get('/', async (req, res) => {
+    const messsages = await Message.findAll()
+
+    res.send(messsages)
+})
 
 messageRoute.get('/{id}', (req, res) => {
     res.send("This is the message/{id} GET route")
 })
 
-messageRoute.post('/{id}', (req, res) => {
-    res.send("This is the message/{id} POST route")
-})
 
 messageRoute.put('/{id}', (req, res) => {
     res.send("This is the message/{id} POST route")
