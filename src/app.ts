313621fd4b7fb37e9679dbf359628251b03f5673
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2023-05-05 15:52:01 +0100

    Setup global middleware

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,29 @@
-import express, { Express, Request, Response } from 'express';
-import cors from 'cors';
+import http from "http";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import passport from "passport";
+import { sessionMiddleware } from "./sessions";
 
+const app = express();
+const server = http.createServer(app);
 
-const app: Express = express();
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+	})
+);
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Test Server');
-  });
-  
-export default app;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(sessionMiddleware);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => {
+	res.send("Test Server");
+});
+
+export default server;
