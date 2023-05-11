6fcfad6f6898cc4c4637075b71aecab878b0eba4
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2023-05-11 10:55:02 +0100

    Update types.ts

--- src/typings/types.ts
+++ src/typings/types.ts
@@ -1,4 +1,16 @@
-export interface ChatMessage {
-    author: string;
-    content: string;
+export interface IncomingMessage {
+	channelId: number;
+	content: string;
+}
+
+export interface OutgoingMessage {
+	id: number;
+	channelId: number;
+	content: string;
+	isEdited: boolean;
+	editedAt: Date;
+	createdAt: Date;
+	user: {
+		username: string;
+	};
 }
