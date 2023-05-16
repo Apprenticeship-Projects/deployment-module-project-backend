d76f1a650ad444f69cb227ea63e3770f264ae5e0
anammSky <ana.morosmarmol@sky.uk>
2023-05-16 12:17:47 +0100

    added register route

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,19 +1,54 @@
 import { Router } from "express";
+import { User } from "../db/models";
+import * as bcrypt from "bcrypt";
+import { body, validationResult } from "express-validator";
+import { SALT_ROUNDS } from "../constants";
 
 export const userRoute = Router();
 
-userRoute.post('/register', (req, res) => {
-    res.send("This is the user/register POST route")
-})
+userRoute.post(
+	"/register",
+	body("email").isEmail().normalizeEmail({ all_lowercase: true }),
+	body("password").isStrongPassword({
+		minLength: 8,
+		minLowercase: 1,
+		minUppercase: 1,
+		minNumbers: 1,
+		minSymbols: 1,
+	}),
+	body("username").isLength({ min: 2, max: 40 }),
+	async (req, res, next) => {
+		try {
+			const { email, username, password } = req.body;
 
-userRoute.get('/me', (req, res) => {
-    res.send("This is the user/me GET route")
-})
+			const userExists = await User.findOne({ where: { email: email } });
 
-userRoute.put('/me', (req, res) => {
-    res.send("This is the user/me PUT route")
-})
+			if (userExists) {
+				return res.status(409).send({ message: "Account already exists" });
+			}
+			const hashPassword = await bcrypt.hash(password, SALT_ROUNDS);
 
-userRoute.delete('/me', (req, res) => {
-    res.send("This is the user/me DELETE route")
-})
+			const createdUser = await User.create({
+				username: username,
+				email: email,
+				password: hashPassword,
+			});
+
+			res.send(createdUser);
+		} catch (error) {
+			next(error);
+		}
+	}
+);
+
+userRoute.get("/me", (req, res) => {
+	res.send("This is the user/me GET route");
+});
+
+userRoute.put("/me", (req, res) => {
+	res.send("This is the user/me PUT route");
+});
+
+userRoute.delete("/me", (req, res) => {
+	res.send("This is the user/me DELETE route");
+});
